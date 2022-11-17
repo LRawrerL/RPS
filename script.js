a06ace9a715f85826ea0629e7e047b8fca5355b3
LRawrerL <yixuanliang05@gmail.com>
2022-11-17 16:24:34 +0800

    Function Single Round
    Created a function that decides who wins

--- script.js
+++ script.js
@@ -5,6 +5,58 @@ function getComputerChoice()
     ai = choice[array_Index];
     console.log(ai);
     console.log(array_Index);
+    return ai;
 }
 
-getComputerChoice();
+function RPS(playerSelect, computerSelection)
+{
+    if (playerSelect == "Rock")
+    {
+        if (computerSelection == "Scissors")
+        {
+            console.log("You win!");
+        }
+        else if (computerSelection == "Paper")
+        {
+            console.log("You lose!");
+        }
+        else
+        {
+            console.log("Its a Draw!");
+        }
+    }
+    if (playerSelect == "Paper")
+    {
+        if (computerSelection == "Rock")
+        {
+            console.log("You win!");
+        }
+        else if (computerSelection == "Scissors")
+        {
+            console.log("You lose!");
+        }
+        else
+        {
+            console.log("Its a Draw!");
+        }
+    }
+    if (playerSelect == "Scissors")
+    {
+        if (computerSelection == "Paper")
+        {
+            console.log("You win!");
+        }
+        else if (computerSelection == "Rock")
+        {
+            console.log("You lose!");
+        }
+        else
+        {
+            console.log("Its a Draw!");
+        }
+    }
+}
+
+computerSelection = getComputerChoice();
+playerSelect = "Scissors";
+RPS(playerSelect, computerSelection);
